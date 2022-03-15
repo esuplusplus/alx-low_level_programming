3ff745580e0f3bbfe988403b5de8917a06bd9955
unknown <edibekulu@gmail.com>
2022-03-15 06:43:46 -0700

    char put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
-#include <studio.h>
+#include "holberton.h"
+#include <unistd.h>
 
 /**
- * main - a simple program that outputs "Holberton"
- * Return: 0 on success
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char str[] = "Holberton\n";
-	int i = 0;
-
-	while (str[i] != '\0')
-		_putchar(str[i++]);
-	return (0);
+	return (write(1, &c, 1));
 }
