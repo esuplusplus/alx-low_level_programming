53509ef84a1b5bec2404c2aaa58571c7c60ba1e4
esuplusplus <edibekulu@gmail.com>
2022-03-15 14:03:28 -0700

    print the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,21 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_alphabet_x10 - a simple program that outputs the alphabet 10 times
- * followed by a newline
- *
- * Return: 0 on success
- */
+  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * Return: Nothing.
+  */
 void print_alphabet_x10(void)
 {
-	int i, j;
-	char str[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+	char c;
 
-	for (i = 0; i < 10; i++)
-		for (j = 0; str[j] != '\0'; j++)
-			_putchar(str[j]);
+	for (i = 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
+
 
